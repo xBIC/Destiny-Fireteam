b97e40f0f9470c0b34cd707716a7027a56de1bcc
Ubuntu <ubuntu@ip-172-31-63-175.ec2.internal>
2015-01-19 23:43:43 +0000

    Setting up framework

---
+++ public/index.php
@@ -0,0 +1,48 @@
+<?php
+try {
+
+    //Register an autoloader
+    $loader = new \Phalcon\Loader();
+    $loader->registerDirs(array(
+        '../app/controllers/',
+        '../app/models/'
+    ))->register();
+
+    //Create a DI
+    $di = new Phalcon\DI\FactoryDefault();
+
+    //Setup the database service
+    $di->set('db', function(){
+        return new \Phalcon\Db\Adapter\Pdo\Mysql(array(
+            "host" => "donald-db.cc9xqfenws6x.us-east-1.rds.amazonaws.com",
+            "username" => "donalddb",
+            "password" => "qCtgIDoeqSUkY2w",
+            "dbname" => "Destiny"
+        ));
+    });
+
+    //Setup the view component
+    $di->set('view', function(){
+        $view = new \Phalcon\Mvc\View();
+        $view->setViewsDir('../app/views/');
+        //$view->registerEngines(array(
+        //	".volt" => 'Phalcon\Mvc\View\Engine\Volt'
+    	//));
+	return $view;
+    });
+
+    //Setup a base URI so that all generated URIs include the "tutorial" folder
+    $di->set('url', function(){
+        $url = new \Phalcon\Mvc\Url();
+        $url->setBaseUri('/');
+        return $url;
+    });
+
+    //Handle the request
+    $application = new \Phalcon\Mvc\Application($di);
+
+    echo $application->handle()->getContent();
+
+} catch(\Phalcon\Exception $e) {
+     echo "PhalconException: ", $e->getMessage();
+}
